2e6159632718089eb7cf4f7a9ffef7623bbe1316
Antti Korteniemi <antti.korteniemi@gmail.com>
2015-07-11 13:38:36 +0300

    brew install script added

---
+++ install/brew_install.sh
@@ -0,0 +1,5 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew tap homebrew/versions
+brew tap caskroom/versions
